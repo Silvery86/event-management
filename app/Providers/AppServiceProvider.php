cbeb9e77b17d25f09722691012c23be9e46310f9
giang <giang8692@gmail.com>
2025-03-02 12:58:25 +0700

    Add: Request Limit config

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,10 @@ namespace App\Providers;
 
 use App\Models\Attendee;
 use App\Models\Event;
+use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\Facades\Request;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -22,6 +25,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        RateLimiter::for('api', function (Request $request){
+            return Limit::perMinute(60)->by($request->user()->id) ?: $request->ip();
+        });
+
         // Gate::define('update-event', function ($user, Event $event) {
         //     return $user->id === $event->user_id;
         // });
